2e00de19f8d937dc2e843c932b0fd2ad51395998
IQRA-ABDI <yourname@domain.com>
2022-12-30 23:18:59 +0300

    I finished the sprint

--- components/Header/index.js
+++ components/Header/index.js
@@ -18,5 +18,39 @@ const headerContent = [
         temp: "98°"
     }
 ];
+function data(title, date, temp) {
+    // step1: create elements
+let header = document.createElement('div')
+let datespan = document.createElement('span')
+let title1 = document.createElement('h1')
+let tempspan = document.createElement('span')
+
+// step2: connect elements 
+header.appendChild(datespan)
+header.appendChild(title1)
+header.appendChild(tempspan)
+
+//step3: add content
+datespan.textContent = date;
+title1.textContent = title;
+tempspan.textContent = temp;
+
+// step4: add classes
+header.classList.add('header')
+datespan.classList.add('date')
+tempspan.classList.add('temp')
+
+
+return header
+}
+
+//SElECTION
+const headerContainer = document.querySelector('.header-container')
+
+// ADD COMMPONENT TO DOM 
+
+headerContent.map(function(item) {
+headerContainer.appendChild(data(item.title,item.date,item.temp))
+})
 
 
